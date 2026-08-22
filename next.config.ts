67929a84672c95d09ba83f0f2e9443d53cf25b32
Nilesh <nilparekh002@gmail.com>
2026-08-22 18:33:18 +0530

    feat: add permanent redirect from vercel deployment domain to custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'maa-industries.vercel.app',
+          },
+        ],
+        destination: 'https://www.maaindustriess.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
